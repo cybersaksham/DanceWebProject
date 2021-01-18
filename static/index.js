b4b2a8073b3ff867a9bf9b4e11ab88b191925b07
Saksham <2020UCP1801@gmail.com>
2021-01-18 15:53:53 +0530

    first commit

---
+++ static/index.js
@@ -0,0 +1,13 @@
+function submit() {
+    // let btn_sub = document.getElementById('btn_sub');
+    // btn_sub.addEventListener('click',function alert(){
+    confirm("Form is submitted successfully. We will contact you soon.");
+    console.log("Submitted");
+    // })
+    return
+}
+
+let btn_sub = document.getElementById('btn_sub');
+btn_sub.addEventListener('click', function run() {
+    console.log("Mouse inside the para");
+})
